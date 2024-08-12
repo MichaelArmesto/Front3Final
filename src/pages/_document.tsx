0ff9d286eaac6ae5bd75ebaac4d708e622020ccd
Humberto Rivero <humbertorivero9@gmail.com>
2024-08-11 22:43:22 -0300

    Add MUI

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,22 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import * as React from 'react';
+import { Html, Head, Main, NextScript, DocumentProps, DocumentContext } from 'next/document';
+import {
+  DocumentHeadTags,
+  DocumentHeadTagsProps,
+  documentGetInitialProps,
+} from '@mui/material-nextjs/v14-pagesRouter';
+import theme, { roboto } from '../theme';
 
-export default function Document() {
+export default function MyDocument(props: DocumentProps & DocumentHeadTagsProps) {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="en" className={roboto.className}>
+      <Head>
+        {/* PWA primary color */}
+        <meta name="theme-color" content={theme.palette.primary.main} />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <meta name="emotion-insertion-point" content="" />
+        <DocumentHeadTags {...props} />
+      </Head>
       <body>
         <Main />
         <NextScript />
@@ -11,3 +24,9 @@ export default function Document() {
     </Html>
   );
 }
+
+MyDocument.getInitialProps = async (ctx: DocumentContext) => {
+  const finalProps = await documentGetInitialProps(ctx);
+  return finalProps;
+};
+
